75a7e358646296e29b31c0bf7d66ec01d9923932
vicky <vcmartin2020@gmail.com>
2021-03-22 11:03:08 -0500

    Problem Four

--- Arrays Practice Problems/index.js
+++ Arrays Practice Problems/index.js
@@ -11,6 +11,12 @@ console.log(arrayToString(["Red", "Green", "White", "Black"]))
 //Problem 3
 sortArray = arr => arr.sort();
 console.log(sortArray([-3, 8, 7, 6, 5, -4, 3, 2, 1]))
-// console.log(sortArray(["Red", "Orange", "Black", "White"]))
+console.log(sortArray(["Red", "Orange", "Black", "White"]))
 
 //Problem 4
+let numbers = [1, 5, 5, 2, 5, 2, 1, 7, 8, 6]
+const removingDuplicates = numbers.filter((number, index)=>{
+    return numbers.indexOf(number) === index;
+})
+
+console.log(removingDuplicates)
